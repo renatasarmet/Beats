f2f5862a3df0767920e6396409d140a26bb65d84
renatasarmet <renatassmendes@hotmail.com>
2017-10-04 17:20:47 -0300

    Criacao tela adicionar contatos

--- app/src/main/java/com/example/android/beats/MainActivity.java
+++ app/src/main/java/com/example/android/beats/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
