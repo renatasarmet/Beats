28db15e1d8d5fcd872419aa6c13fca9fe9ef1efe
renatasarmet <renatassmendes@hotmail.com>
2017-10-04 17:15:19 -0300

    Criacao tela adicionar contatos

--- app/src/main/java/com/example/android/beats/MainActivity.java
+++ app/src/main/java/com/example/android/beats/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.android.beats;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     TextInputEditText editTextUsername;
     TextInputEditText editTextPassword;
     Button buttonLogin;
+    Button buttonAdd;
 
 
     @Override
@@ -24,16 +22,23 @@ public class MainActivity extends AppCompatActivity {
         editTextPassword = (TextInputEditText) findViewById(R.id.editTextPassword);
         buttonLogin = (Button) findViewById(R.id.buttonLogin);
 
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(!editTextUsername.toString().isEmpty() && !editTextPassword.toString().isEmpty()){
-                    setContentView(R.layout.contatos);
+                    setContentView(R.layout.contacts);
                 }
             }
         });
 
-
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.addcontact);
+            }
+        });
 
     }
 }
